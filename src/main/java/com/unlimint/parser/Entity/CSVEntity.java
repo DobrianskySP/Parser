a3b77d6f2a5e4a8c446f1227a63fbd25ff258751
dobrianskysp <ZYXwvu98&>
2021-03-09 22:37:05 +0300

    CSVEntity created

--- src/main/java/com/unlimint/parser/Entity/CSVEntity.java
+++ src/main/java/com/unlimint/parser/Entity/CSVEntity.java
@@ -1,4 +1,22 @@
 package com.unlimint.parser.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class CSVEntity {
+    private Long id;
+    private Double amount;
+    private String currency;
+    private String comment;
+    private String filename;
+    private Long line;
+    private String result;
 }
